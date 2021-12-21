5993d822eb8163f7fee81af288ff2ece4b97adff
吴红星 <wuhongxing0512@zto.com>
2021-12-21 21:26:04 +0800

    Initial Commit

---
+++ StudyMulThread/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyMulThread
+//
+//  Created by 吴红星 on 2021/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
